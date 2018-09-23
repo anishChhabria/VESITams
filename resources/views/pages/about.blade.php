814cbd8c13dc4fea71033ff49df625768a428f03
Anish Chhabria <2017.anish.chhabria@ves.ac.in>
2018-09-23 18:55:18 +0530

    VH1.2

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,22 +1,10 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
+    
+<br><br><br><br><br><br><br>
+    <div class="jubotron text-center"> 
 
-        <title>{{config('app.name')}}</title>
-    </head>
-    <body>
-                 <div class="content">
-                    <div class="jumbotron jumbotron-flud text-center">
-                        <div class="container">
-                            <div class="title m-b-md">
-                            Welcome to VESITams Abouyt
-                            </div>
-                        </div>
-                    </div>
-                </div>
+         Welcome to VESITams About
+    </div>
 
-    </body>
-</html>
+@endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,21 +1,10 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
+<div class="content">
+    <br><br><br><br><br><br><br>
+    <div class="jubotron text-center">     
+    <p>Welcome to VESITams</p>
+    <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+</div>
+@endsection
 
-        <title>{{config('app.name')}}</title>
-    </head>
-    <body>
-            <div class="content">
-                <div class="jumbotron jumbotron-flud text-center">
-                    <div class="container">
-                        <div class="title m-b-md">
-                         Welcome to VESITams
-                        </div>
-                    </div>
-                </div>
-            </div>    
-    </body>
-</html>
